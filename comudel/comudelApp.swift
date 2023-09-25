5c89867ae19238f6be051a842e58151c6020a277
Armando <jarodriguezvera@gmail.com>
2023-09-24 21:36:50 -0600

    Initial Commit

---
+++ comudel/comudelApp.swift
@@ -0,0 +1,17 @@
+//
+//  comudelApp.swift
+//  comudel
+//
+//  Created by Armando Rodríguez on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct comudelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
